75ccf7d719b579063b06b3a625d90cc1f7400bd8
jvjspy <ngochung1762@gmail.com>
2021-06-21 15:20:38 +0700

    Optimize filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import {
-  Box,
-  Center,
-  ChakraProvider,
-  CircularProgress,
-  Container,
-  Flex,
-  SimpleGrid,
-  Spacer,
-  useDisclosure,
+	Box,
+	Center,
+	ChakraProvider,
+	CircularProgress,
+	Container,
+	Flex,
+	SimpleGrid,
+	Spacer,
+	useDisclosure,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { getAll } from "./api/country-api";
@@ -19,95 +19,92 @@ import SearchBar from "./components/SearchBar";
 
 const regions = ["Africa", "Americas", "Asia", "Europe", "Oceania"];
 function App() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [currentCountry, setCurrentCountry] = useState();
-  const [countries, setCountries] = useState([]);
-  const [filterCountries, setFilterCountries] = useState([]);
-  const [isLoading, setIsLoading] = useState();
-  const [selectedFilter, setSelectedFilter] = useState("All");
-  const onSearch = (searchText) => {
-    if (searchText == "") {
-      setFilterCountries(countries);
-    } else {
-      setFilterCountries(
-        countries.filter((c) =>
-          c.name.toLowerCase().includes(searchText.toLowerCase())
-        )
-      );
-    }
-  };
-  const onFilter = (region) => {
-    setSelectedFilter(region);
-    if (region == "All") {
-      setFilterCountries(countries);
-    } else {
-      setFilterCountries(countries.filter((c) => c.region == region));
-    }
-  };
-  useEffect(() => {
-    setIsLoading(true);
-    getAll()
-      .then((data) => {
-        setCountries(data);
-        setFilterCountries(data);
-      })
-      .finally(() => setIsLoading(false));
-  }, []);
-  const onCardClick = (country) => {
-    onOpen();
-    setCurrentCountry(country);
-  };
-  return (
-    <ChakraProvider>
-      <Header />
-      <Center mt="8">
-        <Container maxW="container.xl">
-          <Flex direction={["column", "row"]}>
-            <Box width={["100%", "40%"]}>
-              <SearchBar onSearch={onSearch} />
-            </Box>
-            <Spacer />
-            <Box mt={["8", "unset"]} width={["50%", "auto"]}>
-              <Filter
-                selectedRegion={selectedFilter}
-                regions={regions}
-                onSelect={onFilter}
-              />
-            </Box>
-          </Flex>
-        </Container>
-      </Center>
-      <Container maxW="container.xl" mt="8">
-        {isLoading ? (
-          <Flex justifyContent="center" alignItems="center" height="80vh">
-            <CircularProgress isIndeterminate color="teal.500" size="100px" />
-          </Flex>
-        ) : (
-          <SimpleGrid
-            columns={[1, 2, null, 4]}
-            alignItems="center"
-            justifyItems="center"
-            spacingY="8"
-          >
-            {filterCountries.map((c) => (
-              <CountryCard
-                onClick={onCardClick}
-                country={c}
-                key={c.numericCode}
-              />
-            ))}
-          </SimpleGrid>
-        )}
-      </Container>
-      {currentCountry && (
-        <CountryModal
-          isOpen={isOpen}
-          onClose={onClose}
-          country={currentCountry}
-        />
-      )}
-    </ChakraProvider>
-  );
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	const [currentCountry, setCurrentCountry] = useState();
+	const [countries, setCountries] = useState([]);
+	const [isLoading, setIsLoading] = useState();
+	const [filters, setFilters] = useState({});
+	const doFilter = () => {
+		let res = countries;
+		if (Object.keys(filters).every(key => filters[key] == null)) return res;
+		for (const key in filters) {
+			if (key == "name" && filters[key]) {
+				res = res.filter(c =>
+					c.name.toLowerCase().includes(filters[key].toLowerCase())
+				);
+			} else if (key == "region" && filters[key]) {
+				res = countries.filter(c => c.region == filters[key]);
+			}
+		}
+		return res;
+	};
+	const renderCountries = doFilter();
+	console.log(renderCountries);
+	const onSearch = searchText => {
+		setFilters({ ...filters, name: searchText == "" ? null : searchText });
+	};
+	const onFilterByRegion = region => {
+		setFilters({ ...filters, region: region == "All" ? null : region });
+	};
+	useEffect(() => {
+		setIsLoading(true);
+		getAll()
+			.then(data => {
+				setCountries(data);
+			})
+			.finally(() => setIsLoading(false));
+	}, []);
+	const onCardClick = country => {
+		onOpen();
+		setCurrentCountry(country);
+	};
+	return (
+		<ChakraProvider>
+			<Header />
+			<Center mt="8">
+				<Container maxW="container.xl">
+					<Flex direction={["column", "row"]}>
+						<Box width={["100%", "40%"]}>
+							<SearchBar onSearch={onSearch} />
+						</Box>
+						<Spacer />
+						<Box mt={["8", "unset"]} width={["50%", "auto"]}>
+							<Filter regions={regions} onSelect={onFilterByRegion} />
+						</Box>
+					</Flex>
+				</Container>
+			</Center>
+			<Container maxW="container.xl" mt="8">
+				{isLoading ? (
+					<Flex justifyContent="center" alignItems="center" height="80vh">
+						<CircularProgress isIndeterminate color="teal.500" size="100px" />
+					</Flex>
+				) : (
+					<SimpleGrid
+						columns={[1, 2, null, 4]}
+						alignItems="center"
+						justifyItems="center"
+						spacingY="8"
+					>
+						{renderCountries.map(c => (
+							<CountryCard
+								onClick={onCardClick}
+								country={c}
+								key={c.numericCode}
+							/>
+						))}
+					</SimpleGrid>
+				)}
+			</Container>
+			{currentCountry && (
+				<CountryModal
+					isOpen={isOpen}
+					onClose={onClose}
+					country={currentCountry}
+				/>
+			)}
+		</ChakraProvider>
+	);
 }
 
 export default App;
--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -1,23 +1,29 @@
 import { ChevronDownIcon } from "@chakra-ui/icons";
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import React from "react";
+import { useState } from "react";
 
-export default function Filter({ regions, onSelect, selectedRegion }) {
-  return (
-    <Menu>
-      <MenuButton as={Button} rightIcon={<ChevronDownIcon />} variant="outline">
-        {selectedRegion == "All" ? "Filter by region" : selectedRegion}
-      </MenuButton>
-      <MenuList>
-        <>
-          <MenuItem onClick={() => onSelect("All")}>All regions</MenuItem>
-          {regions.map((r) => (
-            <MenuItem key={r} onClick={() => onSelect(r)}>
-              {r}
-            </MenuItem>
-          ))}
-        </>
-      </MenuList>
-    </Menu>
-  );
+export default function Filter({ regions, onSelect }) {
+	const [selectedRegion, setSelectedRegion] = useState("All");
+	const onClick = r => {
+		setSelectedRegion(r);
+		onSelect(r);
+	};
+	return (
+		<Menu>
+			<MenuButton as={Button} rightIcon={<ChevronDownIcon />} variant="outline">
+				{selectedRegion == "All" ? "Filter by region" : selectedRegion}
+			</MenuButton>
+			<MenuList>
+				<>
+					<MenuItem onClick={() => onClick("All")}>All regions</MenuItem>
+					{regions.map(r => (
+						<MenuItem key={r} onClick={() => onClick(r)}>
+							{r}
+						</MenuItem>
+					))}
+				</>
+			</MenuList>
+		</Menu>
+	);
 }
